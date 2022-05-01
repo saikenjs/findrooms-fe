65bfbc68e1caf8e7c6813ea5e68489e8bbe30ea7
Luc Le <trungluc.dev@gmail.com>
2022-05-01 13:32:53 +0700

    OK

--- src/configs/axios.ts
+++ src/configs/axios.ts
@@ -1,7 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: process.env.NODE_ENV
-    ? 'http://localhost:3001'
-    : 'https://98675f8094954d.lhrtunnel.link',
+  baseURL: 'https://98675f8094954d.lhrtunnel.link',
 });
